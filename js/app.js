03bfb0acb8f93e4af7c073d022168ed52f052bed
Abel Madera <abelmadera1212@gmail.com>
2023-03-09 18:02:57 -0600

    "Chapter 3 complete - Made the ring appended it"

--- js/app.js
+++ js/app.js
@@ -93,11 +93,12 @@ function keepItSecretKeepItSafe() {
   console.log("3: keepItSecretKeepItSafe");
 
   // create a div with an id of `'the-ring'`
-
+  const ringDiv = document.createElement('div')
+  ringDiv.setAttribute('id', 'the-ring')
   // give the div a class of `'magic-imbued-jewelry'`
-
+  ringDiv.classList.add('magic-imbued-jewelry')
   // add the ring as a child of `Frodo`
-
+  document.querySelectorAll('.hobbit')[0].appendChild(ringDiv)
 }
 
 // COMMIT YOUR WORK
